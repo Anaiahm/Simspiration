be5f4b0e8457bca4ceefc0f16127b88517f7fa3c
Anaiahm <anaiahmaddox@gmail.com>
2024-09-03 16:15:29 -0400

    add redirect file

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,69 @@
 import React from 'react';
-// import './Header.css'; 
+import { Link } from 'react-router-dom';
+import { AuthedUserContext } from '../../App';
+import { useContext } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUser, faCog, faMoon, faHeart, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import './Header.css'; 
 
-function Header() {
+function Header({ handleSignout }) {
+    const user = useContext(AuthedUserContext);
   return (
+    <>
+      {user ? (
     <header>
       <div className="header-content">
-        <img src="your-image-url.jpg" alt="Centered Image" className="centered-image" />
+        <img src="https://imgur.com/dqOzWaR.png" alt="Centered Image" className="centered-image" />
         <div className="right-buttons">
-          <button>Dashboard</button>
-          <button>Favorites</button>
-          <button>NightMode</button>
+          <Link to="/" className="icon-link">
+          <FontAwesomeIcon icon={faHome} />
+        </Link>
+        <Link to="/dashboard" className="icon-link">
+          <FontAwesomeIcon icon={faUser} />
+        </Link>
+          <Link to="/favorites" className="icon-link">
+          <FontAwesomeIcon icon={faHeart} />
+        </Link>
+        <Link to="/nightmode" className="icon-link">
+          <FontAwesomeIcon icon={faMoon} />
+        </Link>
+        <Link to="/logout" className="icon-link">
+          <FontAwesomeIcon icon={faSignOutAlt} />
+        </Link>
         </div>
-      </div>
+            </div>
     </header>
+    ) : (
+        <header>
+             <div className="header-content">
+             <img src="https://imgur.com/dqOzWaR.png" alt="Centered Image" className="centered-image" />
+            <Link to="/" className="icon-link">
+          <FontAwesomeIcon icon={faHome} />
+        </Link>
+            <Link to="/nightmode" className="icon-link">
+          <FontAwesomeIcon icon={faMoon} />
+        </Link>
+        </div>
+        </header>
+     )}
+     </>
   );
 }
 
 export default Header;
+
+{/* <button>
+    <FontAwesomeIcon icon={faHome} />
+    </button>
+    <button>
+    <FontAwesomeIcon icon={faUser} />
+    </button>
+    <button>
+    <FontAwesomeIcon icon={faHeart} />
+    </button>
+    <button>
+    <FontAwesomeIcon icon={faMoon} />
+    </button>
+    <button>
+    <FontAwesomeIcon icon={faSignOutAlt} />
+    </button> */}
--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -5,6 +5,22 @@ const Landing = () => {
       <h3>
         Whether you’re crafting dreamy builds, conquering gameplay challenges, spilling secret tips, or uncovering the best cheats, there’s a perfect spot here just for you. Sign Up & Sign in to create, play, and inspire each other in the most fabulous Sim-tastic way!
       </h3>
+      <>
+        <p>CHEATS <img></img></p>
+        <img></img>    
+      </>
+      <>
+        <p>TIPS <img></img></p>
+        <img></img>
+      </>
+      <>
+        <p>BUILD  CHALLENGES <img></img></p>
+        <img></img>
+      </>
+      <>
+        <p>GAMEPLAY CHALLENGES <img></img></p>
+        <img></img>
+      </>
     </main>
   );
 };
